93ebdc5ad4c116c460e5f3f003372a8402e68d0c
mkperez <mkperez@umich.edu>
2019-04-29 18:00:42 -0400

    please push

--- Data/Spont-Speech/all_data_test/test/dict/copy_dict.sh
+++
@@ -1,9 +0,0 @@
-DICT_PATH='/z/mkperez/PremanifestHD/global-dict'
-GF_kaldi='/z/mkperez/PremanifestHD-classification/Data/GF-Passage'
-
-#echo $DATA_ROOT $DICT_PATH
-#exit
-for dir in $GF_kaldi/set*
-do
-	cp -r $DICT_PATH $dir/dict
-done
